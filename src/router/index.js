7eff5a465c6e2235fdf6898133ea7f2af898779c
krisnaxyoga <yogakris.yk@gmail.com>
2021-10-06 06:20:36 +0800

    test frontend

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+    {
+        path: '/',
+        name: 'transaction.index',
+        component: () => import("../views/transaction/Index.vue")
+    },
+    {
+        path: '/edit/:id',
+        name: 'transaction.edit',
+        component: () => import("../views/transaction/Edit.vue")
+    },
+    {
+        path: '/create',
+        name: 'transaction.create',
+        component: () => import("../views/transaction/Create.vue")
+    }
+];
+const router = createRouter({
+    history : createWebHistory(),
+    routes
+});
+export default router;
